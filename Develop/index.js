0a7b2fabb5b1701c6d26f28086da5f061973c0b7
Abdulhalim Zeynu <a.zeynu8@gmail.com>
2021-01-30 14:03:01 -0600

    updated with correct repo

--- Develop/index.js
+++ Develop/index.js
@@ -1,72 +1,52 @@
-const fs = require("fs")
-const inquirer = require("inquirer")
-const generateMarkdown = require("./utils/generateMarkdown");
+var mysql = require("mysql");
+var inq = require("inquirer");
+var table = require("console.table");
+var add = require("./add");
+var update = require("./update");
+var view = require("./view");
 
-// array of questions for user
-const questions = [
-    {
-        type: "input",
-        name: "username",
-        message: "Enter your GitHub username: "
-      },{
-        type: "input",
-        name: "email",
-        message: "Enter your email address: "
-      },
-      {
-        type: "input",
-        name: "title",
-        message: "Enter project name: "
-      },
-      {
-        type: "input",
-        name: "desc",
-        message: "Short description of project: "
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Type of license (MIT): ",
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-        default: "MIT"
-      },
-      {
-        type: "input",
-        name: "install",
-        message: "Command to install dependencies (npm i): ",
-        default: "npm i"
-      },
-      {
-        type: "input",
-        name: "test",
-        message: "Command to run tests (npm test): ",
-        default: "npm test"
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Usage information for users: ",
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: "Contribution guidelines for developers: ",
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "comp_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  exports.start();
+});
+
+exports.start = () => {
+    inq.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "EXIT"                
+            ]
+        }
+    ])
+    .then(function(answer) {
+      if(answer.choice === "View All Employees") {
+        view.viewAllEmployees();
       }
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-    const write = fs.writeFileSync(fileName, data)
-}
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions).then(answers => {
-        const data = generateMarkdown(answers)
-        writeToFile("README.md", data)
-        console.log("Generated README file in working directory")
-    })
-}
-
-// function call to initialize program
-init();
+      else if(answer.choice === "Add Employee") {
+        add.addEmployee();
+      }      
+      else if(answer.choice === "Update Employee Role") {
+        update.updateRole();
+      }
+      else if(answer.choice === "EXIT") {
+        connection.end();
+        return
+      }
+    });
+    
+};
